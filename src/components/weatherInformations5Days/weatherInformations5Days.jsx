298ba74fafd4f74c7d9c88779b80361984856dee
Evaristo GC <evaristogaell@gmail.com>
2024-09-17 23:24:59 +0200

    informations for the next five days - styled

--- src/components/weatherInformations5Days/weatherInformations5Days.jsx
+++ src/components/weatherInformations5Days/weatherInformations5Days.jsx
@@ -19,24 +19,26 @@ function WeatherInformations5Days({ city5Days }) {
     function convertDate(date) {
         return (
             new Date(date * 1000)
-            .toLocaleDateString('pt', {weekday: 'long', day: '2-digit'})
+                .toLocaleDateString('pt', { weekday: 'long', day: '2-digit' })
         )
     }
 
     return (
         <div className='weather-container'>
-            <p>Previsão dos próximos 5 Dias</p>
-            {next5DaysForecast.map(forecast => (
-                <div key={forecast.dt}>
-                    <p>{convertDate(forecast.dt)}</p>
-                    <img src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`} />
-                    <p>{forecast.weather[0].description}</p>
-                    <p>
-                        {Math.round(forecast.main.temp_min)}ºC min. /
-                        {Math.round(forecast.main.temp_max)}ºC máx.
-                    </p>
-                </div>
-            ))}
+            <h3>Previsão dos próximos 5 Dias</h3>
+            <div className="weather-list">
+                {next5DaysForecast.map(forecast => (
+                    <div key={forecast.dt} className='weather-item'>
+                        <p className='forecast-day'>{convertDate(forecast.dt)}</p>
+                        <img src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`} />
+                        <p className='forecast-description'>{forecast.weather[0].description}</p>
+                        <p>
+                            {Math.round(forecast.main.temp_min)}ºC min. /
+                            {Math.round(forecast.main.temp_max)}ºC máx.
+                        </p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
